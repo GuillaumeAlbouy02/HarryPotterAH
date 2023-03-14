abeb87901b2e62893eaf11a4d477b08f7e8bffe6
Guillaume <guillaumealbouy02@gmail.com>
2023-03-14 18:21:26 +0100

    File reading

--- src/main/java/org/HarryPotter/Characters/spells/Spell.java
+++ src/main/java/org/HarryPotter/Characters/spells/Spell.java
@@ -1,4 +1,9 @@
 package org.HarryPotter.Characters.spells;
 
 public class Spell extends AbstractSpell{
+
+    @Override
+    public String toString() {
+        return null;
+    }
 }
--- src/main/java/org/HarryPotter/levels/Obstacle.java
+++
@@ -1,9 +0,0 @@
-package org.HarryPotter.levels;
-
-import org.HarryPotter.Position;
-
-public class Obstacle {
-
-    private Position position;
-    private boolean movable;
-}
